b40e1574cbfd16ee38c36265b2634f9189fb9bf0
Swapnil bhakare <swapnilbhakare7@gmail.com>
2024-02-26 22:24:24 +0530

    socket connection done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import http from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
 
 app.use(
   cors({
@@ -17,19 +29,9 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
-import chatRouter from "./routes/chat.routes.js";
-
-const server = http.createServer(app);
-const io = new Server(server);
-io.on("connection", (socket) => {
-  socket.on("error", (error) => {
-    console.error("Socket error:", error);
-  });
-});
 
 app.use("/api/users", userRouter);
-app.use("/api/chat", chatRouter);
 
 // http://localhost:8000/api/users/send-otp
 
-export default app;
+export default server;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import app from "./app.js";
+import server from "./app.js";
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    server.listen(process.env.PORT || 8000, () => {
       console.log(`Listening on port http://localhost:${process.env.PORT}`);
     });
   })
